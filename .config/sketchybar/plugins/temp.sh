e500294158dfd86e9053ba83e21bbf240fd005a0
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-10-01 02:39:22 +1000

    yay

--- .config/sketchybar/plugins/temp.sh
+++ .config/sketchybar/plugins/temp.sh
@@ -2,7 +2,7 @@
 
 # filter="PMU tdev"
 filter="PMU tcal"
-sensor_output=$(./plugins/macos-temp-tool -f "${filter}" -a | awk '{print $NF}')
+# sensor_output=$(./plugins/macos-temp-tool -f "${filter}" -a | awk '{print $NF}')
 sensor_output=$(smctemp -g)
 LABEL="${sensor_output}°C"
 
